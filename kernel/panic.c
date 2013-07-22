1f8155adbcfb7908d11cc763c1784badc53d2d7f
Bongkyu Kim <bongkyu.kim@lge.com>
2013-07-21 23:26:08 -0500

    panic: force console_unlock() in panic()
    If panic() is called when console is locked,
    oops message can't be printed in console.
    Especially, android ram_console doesn't have any information in this case.
    Thus, console_unlock() in panic() if console is locked.
    
     Conflicts:
    	kernel/panic.c
    
    Change-Id: Ifbac48875ac189c35ed380b67734d0411bed2c08
    Signed-off-by: Bongkyu Kim <bongkyu.kim@lge.com>
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- kernel/panic.c
+++ kernel/panic.c
@@ -28,6 +28,7 @@
 #include <linux/init.h>
 #include <linux/nmi.h>
 #include <linux/crash_notes.h>
+#include <linux/console.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/exception.h>
@@ -142,6 +143,9 @@ void panic(const char *fmt, ...)
 	smp_send_stop();
 
 	kmsg_dump(KMSG_DUMP_PANIC);
+
+	if (is_console_locked())
+		console_unlock();
 
 	atomic_notifier_call_chain(&panic_notifier_list, 0, buf);
 
