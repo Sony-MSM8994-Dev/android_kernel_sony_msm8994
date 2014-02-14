72f6f103f4f432b47209a037dea034e432134d33
Weijie Yang <weijie.yang@samsung.com>
2014-02-14 14:03:56 +0800

    UPSTREAM: staging: android: lowmemorykiller: set TIF_MEMDIE before send kill sig
    (cherry picked from commit 6bc2b856bb7c49f238914d965c0b1057ec78226e)
    
    Set TIF_MEMDIE tsk_thread flag before send kill signal to the
    selected thread. This is to fit a usual code sequence and avoid
    potential race issue.
    
    Signed-off-by: Weijie Yang <weijie.yang@samsung.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Git-commit: 76fc4536faaac9644151fb8a38731c8cf4ab85c4
    Git-repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>
    [Resolved trivial merge conflicts]
    Signed-off-by: GreyLeshy <andreiskull@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -613,8 +613,8 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		}
 
 		lowmem_deathpending_timeout = jiffies + HZ;
-		send_sig(SIGKILL, selected, 0);
 		set_tsk_thread_flag(selected, TIF_MEMDIE);
+		send_sig(SIGKILL, selected, 0);
 		rem -= selected_tasksize;
 		rcu_read_unlock();
 		trace_lmk_sigkill(selected->pid, selected->comm,
