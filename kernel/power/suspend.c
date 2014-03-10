c8f38a82de20346bd8d3d858adeecc6415503776
Viresh Kumar <viresh.kumar@linaro.org>
2014-03-10 19:31:51 +0530

    PM / suspend: Remove unnecessary !!
    Double ! or !! are normally required to get 0 or 1 out of a expression. A
    comparision always returns 0 or 1 and hence there is no need to apply double !
    over it again.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -51,7 +51,7 @@ static const struct platform_freeze_ops *freeze_ops;
 
 static bool need_suspend_ops(suspend_state_t state)
 {
-	return !!(state > PM_SUSPEND_FREEZE);
+	return state > PM_SUSPEND_FREEZE;
 }
 
 static DECLARE_WAIT_QUEUE_HEAD(suspend_freeze_wait_head);
