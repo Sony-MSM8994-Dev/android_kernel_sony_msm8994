e0819ede7e843e5fcdf744b649479c5726ac3a3b
Robert P. J. Day <rpjday@crashcourse.ca>
2014-05-16 04:36:13 -0400

    miscdevice.h: Simple syntax fix to make pointers consistent.
    Signed-off-by: Robert P. J. Day <rpjday@crashcourse.ca>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/miscdevice.h
+++ include/linux/miscdevice.h
@@ -61,7 +61,7 @@ struct miscdevice  {
 	umode_t mode;
 };
 
-extern int misc_register(struct miscdevice * misc);
+extern int misc_register(struct miscdevice *misc);
 extern int misc_deregister(struct miscdevice *misc);
 
 #define MODULE_ALIAS_MISCDEV(minor)				\
