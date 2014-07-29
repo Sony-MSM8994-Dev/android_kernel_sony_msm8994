db64138bd618abb6e03cdb110c8739a41d1f3a25
Jarod Wilson <jarod@redhat.com>
2014-07-29 15:47:56 -0400

    crypto: testmgr - add missing spaces to drbg error strings
    There are a few missing spaces in the error text strings for
    drbg_cavs_test, trivial fix.
    
    CC: "David S. Miller" <davem@davemloft.net>
    CC: linux-crypto@vger.kernel.org
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Acked-by: Stephan Mueller <smueller@chronox.de>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -1649,7 +1649,7 @@ static int drbg_cavs_test(struct drbg_testvec *test, int pr,
 
 	drng = crypto_alloc_rng(driver, type, mask);
 	if (IS_ERR(drng)) {
-		printk(KERN_ERR "alg: drbg: could not allocate DRNG handle for"
+		printk(KERN_ERR "alg: drbg: could not allocate DRNG handle for "
 		       "%s\n", driver);
 		kzfree(buf);
 		return -ENOMEM;
@@ -1674,7 +1674,7 @@ static int drbg_cavs_test(struct drbg_testvec *test, int pr,
 			buf, test->expectedlen, &addtl);
 	}
 	if (ret <= 0) {
-		printk(KERN_ERR "alg: drbg: could not obtain random data for"
+		printk(KERN_ERR "alg: drbg: could not obtain random data for "
 		       "driver %s\n", driver);
 		goto outbuf;
 	}
@@ -1689,7 +1689,7 @@ static int drbg_cavs_test(struct drbg_testvec *test, int pr,
 			buf, test->expectedlen, &addtl);
 	}
 	if (ret <= 0) {
-		printk(KERN_ERR "alg: drbg: could not obtain random data for"
+		printk(KERN_ERR "alg: drbg: could not obtain random data for "
 		       "driver %s\n", driver);
 		goto outbuf;
 	}
