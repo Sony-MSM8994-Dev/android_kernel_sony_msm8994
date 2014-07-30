d611fcd153898faed26b5c2a591fd12414b17c12
Asutosh Das <asutoshd@codeaurora.org>
2014-07-30 14:10:20 +0530

    mmc: cmdq: support for command queue enabled host
    This patch adds CMDQ support for command-queue compatible hosts.
    
    Command queue is added in eMMC-5.1 specification.
    This enables the controller to process upto 32 requests at a time.
    
    Change-Id: I0486495ef57c64bf8427e917daeb184c69b8dc73
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    Signed-off-by: Sujit Reddy Thumma <sthumma@codeaurora.org>
    Signed-off-by: Konstantin Dorfman <kdorfman@codeaurora.org>
    [Resolved trivial merge conflicts]
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -519,6 +519,13 @@ struct mmc_host {
 	bool			card_clock_off;
 	bool			wakeup_on_idle;
 	struct mmc_cmdq_context_info	cmdq_ctx;
+	/*
+	 * several cmdq supporting host controllers are extensions
+	 * of legacy controllers. This variable can be used to store
+	 * a reference to the cmdq extension of the existing host
+	 * controller.
+	 */
+	void *cmdq_private;
 #ifdef CONFIG_MMC_BLOCK_DEFERRED_RESUME
 	bool			rescan_exec_flag;
 #endif
@@ -543,6 +550,11 @@ extern void mmc_set_embedded_sdio_data(struct mmc_host *host,
 static inline void *mmc_priv(struct mmc_host *host)
 {
 	return (void *)host->private;
+}
+
+static inline void *mmc_cmdq_private(struct mmc_host *host)
+{
+	return host->cmdq_private;
 }
 
 #define mmc_host_is_spi(host)	((host)->caps & MMC_CAP_SPI)
