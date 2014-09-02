2a2428b17797cc83edd8fba6273b24bc31f7c890
Jeff Moyer <jmoyer@redhat.com>
2014-09-02 13:17:00 -0400

    aio: add missing smp_rmb() in read_events_ring
    We ran into a case on ppc64 running mariadb where io_getevents would
    return zeroed out I/O events.  After adding instrumentation, it became
    clear that there was some missing synchronization between reading the
    tail pointer and the events themselves.  This small patch fixes the
    problem in testing.
    
    Thanks to Zach for helping to look into this, and suggesting the fix.
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Benjamin LaHaise <bcrl@kvack.org>
    Cc: stable@vger.kernel.org
    [Backported to 3.10: Adjust context]
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- fs/aio.c
+++ fs/aio.c
@@ -718,6 +718,12 @@ static long aio_read_events_ring(struct kioctx *ctx,
 	head = ring->head;
 	kunmap_atomic(ring);
 
+	/*
+	 * Ensure that once we've read the current tail pointer, that
+	 * we also see the events that were stored up to the tail.
+	 */
+	smp_rmb();
+
 	pr_debug("h%u t%u m%u\n", head, ctx->tail, ctx->nr_events);
 
 	if (head == ctx->tail)
