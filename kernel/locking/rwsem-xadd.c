b51ef8b8d5c0765872947aade9cd25a275a5f2bf
Davidlohr Bueso <dave@stgolabs.net>
2014-09-11 22:34:25 -0700

    locking/rwsem: Move EXPORT_SYMBOL() lines to follow function definition
    rw-semaphore is the only type of lock doing this ugliness of
    exporting at the end of the file.
    
    Signed-off-by: Davidlohr Bueso <dbueso@suse.de>
    Cc: dave@stgolabs.net
    Cc: peterz@infradead.org
    Link: http://lkml.kernel.org/r/1410500066-5909-1-git-send-email-dave@stgolabs.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/locking/rwsem-xadd.c
+++ kernel/locking/rwsem-xadd.c
@@ -235,6 +235,7 @@ struct rw_semaphore __sched *rwsem_down_read_failed(struct rw_semaphore *sem)
 
 	return sem;
 }
+EXPORT_SYMBOL(rwsem_down_read_failed);
 
 /*
  * wait until we successfully acquire the write lock
@@ -296,6 +297,7 @@ struct rw_semaphore __sched *rwsem_down_write_failed(struct rw_semaphore *sem)
 
 	return sem;
 }
+EXPORT_SYMBOL(rwsem_down_write_failed);
 
 /*
  * handle waking up a waiter on the semaphore
@@ -315,6 +317,7 @@ struct rw_semaphore *rwsem_wake(struct rw_semaphore *sem)
 
 	return sem;
 }
+EXPORT_SYMBOL(rwsem_wake);
 
 /*
  * downgrade a write lock into a read lock
@@ -335,8 +338,4 @@ struct rw_semaphore *rwsem_downgrade_wake(struct rw_semaphore *sem)
 
 	return sem;
 }
-
-EXPORT_SYMBOL(rwsem_down_read_failed);
-EXPORT_SYMBOL(rwsem_down_write_failed);
-EXPORT_SYMBOL(rwsem_wake);
 EXPORT_SYMBOL(rwsem_downgrade_wake);
