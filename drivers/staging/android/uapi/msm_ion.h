b0d1c06bd93164c601fd348c679fdaf504bb40d6
Neeti Desai <neetid@codeaurora.org>
2014-11-14 17:42:49 -0800

    msm: ion: Add system secure heap
    Add infrastructure to support a new system secure
    heap, needed for secure use cases. The new heap
    essentially allocates from the system heap, and
    assigns the memory to the correct VM before
    returning back to the client.
    
    Change-Id: I2cd900e4399eecc3a9510139c4ebea0b713c39fc
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>
    [imaund@codeaurora.org: resolved context conflicts]
    Signed-off-by: Ian Maund <imaund@codeaurora.org>
    (cherry picked from commit 54584acdad15ca927bd28fbe754bf3f9671f8bd2)

--- drivers/staging/android/uapi/msm_ion.h
+++ drivers/staging/android/uapi/msm_ion.h
@@ -6,6 +6,7 @@
 enum msm_ion_heap_types {
 	ION_HEAP_TYPE_MSM_START = ION_HEAP_TYPE_CUSTOM + 1,
 	ION_HEAP_TYPE_SECURE_DMA = ION_HEAP_TYPE_MSM_START,
+	ION_HEAP_TYPE_SYSTEM_SECURE,
 	ION_HEAP_TYPE_REMOVED,
 	/*
 	 * if you add a heap type here you should also add it to
@@ -130,8 +131,8 @@ enum cp_mem_usage {
 #define ION_MM_FIRMWARE_HEAP_NAME	"mm_fw"
 #define ION_PIL1_HEAP_NAME  "pil_1"
 #define ION_PIL2_HEAP_NAME  "pil_2"
-#define ION_SECURE_HEAP_NAME	"secure_heap"
 #define ION_QSECOM_HEAP_NAME	"qsecom"
+#define ION_SECURE_HEAP_NAME	"secure_heap"
 
 #define ION_SET_CACHED(__cache)		(__cache | ION_FLAG_CACHED)
 #define ION_SET_UNCACHED(__cache)	(__cache & ~ION_FLAG_CACHED)
