2cf2a0d89fe2aec12637bfeb0ae612427f62d802
Lai Jiangshan <laijs@cn.fujitsu.com>
2015-01-13 15:30:34 +0800

    rcu_tree: Avoid touching rnp->completed when a new GP is started
    In rcu_gp_init(), rnp->completed equals to rsp->completed in THEORY,
    we don't need to touch it normally.  If something goes wrong,
    it will complain and fixup rnp->completed and avoid oops.
    This commit thus avoids the normal needless store to rnp->completed.
    
    Change-Id: I0c851f96bb2ae7d2a3fb857d010447b1ec9d9d37
    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -1451,8 +1451,8 @@ static int rcu_gp_init(struct rcu_state *rsp)
 		rcu_preempt_check_blocked_tasks(rnp);
 		rnp->qsmask = rnp->qsmaskinit;
 		ACCESS_ONCE(rnp->gpnum) = rsp->gpnum;
-		WARN_ON_ONCE(rnp->completed != rsp->completed);
-		ACCESS_ONCE(rnp->completed) = rsp->completed;
+		if (WARN_ON_ONCE(rnp->completed != rsp->completed))
+			ACCESS_ONCE(rnp->completed) = rsp->completed;
 		if (rnp == rdp->mynode)
 			rcu_start_gp_per_cpu(rsp, rnp, rdp);
 		rcu_preempt_boost_start_gp(rnp);
