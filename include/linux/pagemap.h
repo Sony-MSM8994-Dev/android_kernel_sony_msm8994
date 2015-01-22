6708fe87c7717af804a0a8136a50f4dbefb824c5
Theodore Ts'o <tytso@mit.edu>
2015-01-22 12:09:47 -0500

    mm: add find_get_page_flags() for 3.18 ext4 backport
    Change-Id: I1bf562a302cae211c0b28fb3872f8ee15660c182
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Theodore Ts'o <tytso@google.com>

--- include/linux/pagemap.h
+++ include/linux/pagemap.h
@@ -264,8 +264,16 @@ static inline struct page *page_cache_alloc_readahead(struct address_space *x)
 
 typedef int filler_t(void *, struct page *);
 
-extern struct page * find_get_page(struct address_space *mapping,
-				pgoff_t index);
+extern struct page * find_get_page_flags(struct address_space *mapping,
+					 pgoff_t index, int fgp_flags);
+
+#define FGP_ACCESSED		0x00000001
+
+static inline struct page* find_get_page(struct address_space *mapping,
+					 pgoff_t index)
+{
+	return find_get_page_flags(mapping, index, 0);
+}
 pgoff_t page_cache_next_hole(struct address_space *mapping,
                              pgoff_t index, unsigned long max_scan);
 
