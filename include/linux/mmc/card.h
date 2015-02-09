b3cdbcf302a931983970fd9cb74b01709fd12215
Sahitya Tummala <stummala@codeaurora.org>
2015-02-09 10:45:18 +0530

    mmc: core: Update PON based on the system state
    As per eMMC specification, the PON (Power Off Notification)
    must be sent by host to the card before turning off the power.
    This will allow card to prepare itself for the power off and
    may even reduce the initialization of eMMC upon next boot-up.
    
    Send long PON during system power off and send short PON during
    system reboot to reduce the reboot latency.
    
    Change-Id: If4188b8b80aaa0e6c4e00e1807aa9589d5e7efdb
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    Conflict is due to CONFIG_MMC_CMD_DEBUG.
    	include/linux/mmc/card.h
    Signed-off-by: GreyLeshy <andreiskull@gmail.com>

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -348,6 +348,10 @@ struct mmc_cmd_stats {
 };
 #endif
 
+enum mmc_pon_type {
+	MMC_LONG_PON = 1,
+	MMC_SHRT_PON,
+};
 /*
  * MMC device
  */
@@ -433,7 +437,7 @@ struct mmc_card {
 	struct device_attribute rpm_attrib;
 	unsigned int		idle_timeout;
 	struct notifier_block        reboot_notify;
-	bool issue_long_pon;
+	enum mmc_pon_type pon_type;
 	u8 *cached_ext_csd;
 #ifdef CONFIG_MMC_CMD_DEBUG
 	struct mmc_cmd_stats cmd_stats;
@@ -712,5 +716,5 @@ extern struct mmc_wr_pack_stats *mmc_blk_get_packed_statistics(
 			struct mmc_card *card);
 extern void mmc_blk_init_packed_statistics(struct mmc_card *card);
 extern void mmc_blk_disable_wr_packing(struct mmc_queue *mq);
-extern int mmc_send_long_pon(struct mmc_card *card);
+extern int mmc_send_pon(struct mmc_card *card);
 #endif /* LINUX_MMC_CARD_H */
