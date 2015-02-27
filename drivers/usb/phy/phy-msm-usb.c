227b9349b50a9f7f543f80dcb91bef55c32d88a1
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2015-02-27 12:41:11 +0530

    USB: phy-msm: Add support for enabling HW fix for endpoint prime failure
    When system clock is running at higher frequencies (<= 100MHz), sometimes
    USB endpoint priming is failing and leading to USB functionality failure.
    On new platforms, Hardware fix is available for fixing endpoint prime
    failure through chickenbit in USB register. Hence add support for enabling
    Hardware fix for endpoint prime failure. Also bump up system clock to max
    when Hardware fix for prime failure is enabled.
    
    Change-Id: Ic53e36c9a08a7e9b385eb1038d6fa9cc85425b21
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -775,6 +775,10 @@ static int msm_otg_reset(struct usb_phy *phy)
 		writel_relaxed(readl_relaxed(USB_OTGSC) & ~(OTGSC_IDPU),
 								USB_OTGSC);
 
+	if (pdata->enable_epprime_fix)
+		writel_relaxed(readl_relaxed(USB_GENCONFIG2) |
+				GENCFG2_TX_BUF_PREFETCH_FIX_EN, USB_GENCONFIG2);
+
 	return 0;
 }
 
@@ -4658,6 +4662,7 @@ static struct platform_device *msm_otg_add_pdev(
 		ci_pdata.enable_ahb2ahb_bypass =
 				otg_pdata->enable_ahb2ahb_bypass;
 		ci_pdata.system_clk = otg_pdata->system_clk;
+		ci_pdata.enable_epprime_fix = otg_pdata->enable_epprime_fix;
 		retval = platform_device_add_data(pdev, &ci_pdata,
 			sizeof(ci_pdata));
 		if (retval)
@@ -5055,6 +5060,9 @@ struct msm_otg_platform_data *msm_otg_dt_to_pdata(struct platform_device *pdev)
 	pdata->emulation = of_property_read_bool(node,
 						"qcom,emulation");
 
+	pdata->enable_epprime_fix = of_property_read_bool(node,
+					"qcom,boost-sysclk-with-epprime-fix");
+
 	return pdata;
 }
 
@@ -5096,11 +5104,17 @@ static int msm_otg_probe(struct platform_device *pdev)
 	}
 
 	/*
-	 * Get Max supported clk frequency for USB Core CLK and request
-	 * to set the same.
+	 * USB Core CLK can run at max freq if EP prime fix is present. If that
+	 * is the case, get Max supported clk frequency for USB Core CLK and
+	 * request to set the same. Otherwise set USB Core CLK to defined
+	 * default value.
 	 */
-	motg->core_clk_rate = clk_round_rate(motg->core_clk,
-		USB_DEFAULT_SYSTEM_CLOCK);
+	if (of_property_read_bool(pdev->dev.of_node,
+					"qcom,boost-sysclk-with-epprime-fix"))
+		motg->core_clk_rate = clk_round_rate(motg->core_clk, LONG_MAX);
+	else
+		motg->core_clk_rate = clk_round_rate(motg->core_clk,
+						USB_DEFAULT_SYSTEM_CLOCK);
 	if (IS_ERR_VALUE(motg->core_clk_rate)) {
 		dev_err(&pdev->dev, "fail to get core clk max freq.\n");
 	} else {
