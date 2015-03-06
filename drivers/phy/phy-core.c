dd7272ea32d048bd7c10aa5c3ec16ba7a24bc2b6
Axel Lin <axel.lin@ingics.com>
2015-03-06 15:55:10 +0800

    phy: core: Fixup return value of phy_exit when !pm_runtime_enabled
    [ Upstream commit 736b67a32062240592aad49033859f9712dd18ca ]
    
    When phy_pm_runtime_get_sync() returns -ENOTSUPP, phy_exit() also returns
    -ENOTSUPP if !phy->ops->exit. Fix it.
    Also move the code to override ret close to the code we got ret.
    I think it is less error prone this way.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Acked-by: Roger Quadros <rogerq@ti.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    [Upstream to 3.10]
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- drivers/phy/phy-core.c
+++ drivers/phy/phy-core.c
@@ -171,6 +171,7 @@ int phy_init(struct phy *phy)
 	ret = phy_pm_runtime_get_sync(phy);
 	if (ret < 0 && ret != -ENOTSUPP)
 		return ret;
+	ret = 0; /* Override possible ret == -ENOTSUPP */
 
 	mutex_lock(&phy->mutex);
 	if (phy->init_count == 0 && phy->ops->init) {
@@ -179,8 +180,6 @@ int phy_init(struct phy *phy)
 			dev_err(&phy->dev, "phy init failed --> %d\n", ret);
 			goto out;
 		}
-	} else {
-		ret = 0; /* Override possible ret == -ENOTSUPP */
 	}
 	++phy->init_count;
 
@@ -201,6 +200,7 @@ int phy_exit(struct phy *phy)
 	ret = phy_pm_runtime_get_sync(phy);
 	if (ret < 0 && ret != -ENOTSUPP)
 		return ret;
+	ret = 0; /* Override possible ret == -ENOTSUPP */
 
 	mutex_lock(&phy->mutex);
 	if (phy->init_count == 1 && phy->ops->exit) {
@@ -229,6 +229,7 @@ int phy_power_on(struct phy *phy)
 	ret = phy_pm_runtime_get_sync(phy);
 	if (ret < 0 && ret != -ENOTSUPP)
 		return ret;
+	ret = 0; /* Override possible ret == -ENOTSUPP */
 
 	mutex_lock(&phy->mutex);
 	if (phy->power_count == 0 && phy->ops->power_on) {
@@ -237,8 +238,6 @@ int phy_power_on(struct phy *phy)
 			dev_err(&phy->dev, "phy poweron failed --> %d\n", ret);
 			goto out;
 		}
-	} else {
-		ret = 0; /* Override possible ret == -ENOTSUPP */
 	}
 	++phy->power_count;
 	mutex_unlock(&phy->mutex);
