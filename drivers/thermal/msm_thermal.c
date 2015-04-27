f4821ee954a456faf19524ff6c63598eb99a7eae
Ram Chandrasekar <rkumbako@codeaurora.org>
2015-04-27 14:55:15 -0600

    msm: thermal: Retry on failure to hotplug
    Add support in thermal driver to retry hotplug
    request on failure to fulfill the request.
    
    This retrying will be disabled when the device
    enters suspend and will resume after the device
    exits from suspend.
    
    Change-Id: I9e2c305754465eb458566c0224804c5ad4bda240
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -62,6 +62,7 @@
 #define MSM_TSENS_PRINT  "log_tsens_temperature"
 #define CPU_BUF_SIZE 64
 #define CPU_DEVICE "cpu%d"
+#define HOTPLUG_RETRY_INTERVAL_MS 100
 
 #define THERM_CREATE_DEBUGFS_DIR(_node, _name, _parent, _ret) \
 	do { \
@@ -81,7 +82,7 @@
 } while (0)
 
 static struct msm_thermal_data msm_thermal_info;
-static struct delayed_work check_temp_work;
+static struct delayed_work check_temp_work, retry_hotplug_work;
 static bool core_control_enabled;
 static uint32_t cpus_offlined;
 static cpumask_var_t cpus_previously_online;
@@ -132,6 +133,7 @@ static bool therm_reset_enabled;
 static bool online_core;
 static bool cluster_info_probed;
 static bool cluster_info_nodes_called;
+static bool in_suspend, retry_in_progress;
 static int *tsens_id_map;
 static DEFINE_MUTEX(vdd_rstr_mutex);
 static DEFINE_MUTEX(psm_mutex);
@@ -504,11 +506,15 @@ static int msm_thermal_suspend_callback(
 	case PM_HIBERNATION_PREPARE:
 	case PM_SUSPEND_PREPARE:
 		msm_thermal_update_freq(false, true);
+		in_suspend = true;
+		retry_in_progress = false;
+		cancel_delayed_work_sync(&retry_hotplug_work);
 		break;
 
 	case PM_POST_HIBERNATION:
 	case PM_POST_SUSPEND:
 		msm_thermal_update_freq(false, false);
+		in_suspend = false;
 		if (hotplug_task)
 			complete(&hotplug_notify_complete);
 		else
@@ -2397,6 +2403,17 @@ static void therm_reset_notify(struct therm_threshold *thresh_data)
 					thresh_data->threshold);
 }
 
+static void retry_hotplug(struct work_struct *work)
+{
+	mutex_lock(&core_control_mutex);
+	if (retry_in_progress) {
+		pr_debug("Retrying hotplug\n");
+		retry_in_progress = false;
+		complete(&hotplug_notify_complete);
+	}
+	mutex_unlock(&core_control_mutex);
+}
+
 #ifdef CONFIG_SMP
 static void __ref do_core_control(long temp)
 {
@@ -2465,6 +2482,7 @@ static int __ref update_offline_cores(int val)
 	uint32_t cpu = 0;
 	int ret = 0;
 	uint32_t previous_cpus_offlined = 0;
+	bool pend_hotplug_req = false;
 
 	if (!core_control_enabled)
 		return 0;
@@ -2479,8 +2497,10 @@ static int __ref update_offline_cores(int val)
 			trace_thermal_pre_core_offline(cpu);
 			ret = cpu_down(cpu);
 			if (ret) {
-				pr_err("Unable to offline CPU%d. err:%d\n",
+				pr_err_ratelimited(
+					"Unable to offline CPU%d. err:%d\n",
 					cpu, ret);
+				pend_hotplug_req = true;
 			} else {
 				struct device *cpu_device = get_cpu_device(cpu);
 				kobject_uevent(&cpu_device->kobj, KOBJ_OFFLINE);
@@ -2501,7 +2521,9 @@ static int __ref update_offline_cores(int val)
 				pr_debug("Onlining CPU%d is vetoed\n", cpu);
 			} else if (ret) {
 				cpus_offlined |= BIT(cpu);
-				pr_err("Unable to online CPU%d. err:%d\n",
+				pend_hotplug_req = true;
+				pr_err_ratelimited(
+					"Unable to online CPU%d. err:%d\n",
 					cpu, ret);
 			} else {
 				struct device *cpu_device = get_cpu_device(cpu);
@@ -2512,6 +2534,13 @@ static int __ref update_offline_cores(int val)
 				cpumask_test_cpu(cpu, cpu_online_mask));
 		}
 	}
+
+	if (pend_hotplug_req && !in_suspend && !retry_in_progress) {
+		retry_in_progress = true;
+		schedule_delayed_work(&retry_hotplug_work,
+			msecs_to_jiffies(HOTPLUG_RETRY_INTERVAL_MS));
+	}
+
 	return ret;
 }
 
@@ -4622,6 +4651,7 @@ int msm_thermal_init(struct msm_thermal_data *pdata)
 
 	register_reboot_notifier(&msm_thermal_reboot_notifier);
 	pm_notifier(msm_thermal_suspend_callback, 0);
+	INIT_DELAYED_WORK(&retry_hotplug_work, retry_hotplug);
 	INIT_DELAYED_WORK(&check_temp_work, check_temp);
 	schedule_delayed_work(&check_temp_work, 0);
 
