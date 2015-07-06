a70cf7cf254a46881329e79cebd57bb8148b0b03
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-07-06 19:16:50 -0400

    cpufreq: lionfish: standalone implementation
    This is a new governor that is optimized for smartphones and similar
    battery powered devices. It combines traits of the conservative,
    ondemand, and interactive governors, and aims to maintain CPU
    frequencies inside an intermediate range with optimal performance per
    watt.
    
    It jumps straight to an intermediate frequency when overloaded at low
    speed to avoid lag. It jumps to full speed when overloaded above the
    intermediate frequency. For moderate loads, it votes to raise, lower,
    or maintain the frequency each sampling period. When enough votes to
    change the frequency accumulate, it changes the frequency. By default,
    this governor also brings down the frequency more aggresively than the
    conservative governor, aiming to maintain average CPU load between 40%
    and 80%.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -456,6 +456,9 @@ extern struct cpufreq_governor cpufreq_gov_bioshock;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX_EPS)
 extern struct cpufreq_governor cpufreq_gov_smartmax_eps;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax_eps)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
+extern struct cpufreq_governor cpufreq_gov_lionfish;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lionfish)
 #endif
 
 /*********************************************************************
