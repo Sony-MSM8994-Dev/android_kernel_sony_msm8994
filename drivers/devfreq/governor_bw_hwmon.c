959a39d47c99672b026b3a09911cd86d4ab9c3f4
Saravana Kannan <skannan@codeaurora.org>
2015-08-18 14:28:08 -0700

    PM / devfreq: bw_hwmon: Remove debug logs from high frequency code path
    Using debug logs for the short samples would result in excessive logging
    and also have an impact on CPU power and performance. The tracepoint that's
    already there is a much better alternative. So, delete the debug logs.
    
    Change-Id: Idd1d634f510381d14637f3b84588a118572f3f16
    Signed-off-by: Saravana Kannan <skannan@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/devfreq/governor_bw_hwmon.c
+++ drivers/devfreq/governor_bw_hwmon.c
@@ -231,9 +231,6 @@ int bw_hwmon_sample_end(struct bw_hwmon *hwmon)
 	node->wake = wake;
 	spin_unlock_irqrestore(&irq_lock, flags);
 
-	dev_dbg(df->dev.parent, "MB/s: %5lu, us:%6d, wake: %d\n",
-		mbps, us, wake);
-
 	return wake;
 }
 
