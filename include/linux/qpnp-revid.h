3915388f5c5f7165cfc1e3e5365c6111a53d4f7b
Xiaozhe Shi <xiaozhes@codeaurora.org>
2015-08-19 16:08:43 -0700

    msm: rev-id: add pmic names to the revid_data structure
    Add the pmic names to the revid structure for consumers.
    
    Change-Id: I4887a2bd736d2568d23b0e6dd774436e064379e9
    Signed-off-by: Xiaozhe Shi <xiaozhes@codeaurora.org>

--- include/linux/qpnp-revid.h
+++ include/linux/qpnp-revid.h
@@ -147,12 +147,13 @@
 #define PMI8994_V2P0_SUBTYPE	0x0A
 
 struct pmic_revid_data {
-	u8	rev1;
-	u8	rev2;
-	u8	rev3;
-	u8	rev4;
-	u8	pmic_type;
-	u8	pmic_subtype;
+	u8		rev1;
+	u8		rev2;
+	u8		rev3;
+	u8		rev4;
+	u8		pmic_type;
+	u8		pmic_subtype;
+	const char	*pmic_name;
 };
 
 #ifdef CONFIG_QPNP_REVID
