25f003bddb23ee49758e0152d66e2c86ddda0da1
David Collins <collinsd@codeaurora.org>
2015-08-25 10:52:38 -0700

    regulator: core: put debugfs consumer handle when unregistering a regulator
    Ensure that regulator_put() is called on the core debugfs
    consumer pointer for a given regulator when the debugfs interface
    is no longer needed.  This includes inside of the
    regulator_unregister() function as well as in the error return
    path of rdev_init_debugfs().
    
    Change-Id: I10563ae1716f31bdc5840d22633fdbfe278330f1
    Signed-off-by: David Collins <collinsd@codeaurora.org>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -3882,6 +3882,14 @@ static const struct file_operations reg_consumers_fops = {
 	.release	= single_release,
 };
 
+static void rdev_deinit_debugfs(struct regulator_dev *rdev)
+{
+	if (!IS_ERR_OR_NULL(rdev)) {
+		debugfs_remove_recursive(rdev->debugfs);
+		regulator_put(rdev->debug_consumer);
+	}
+}
+
 static void rdev_init_debugfs(struct regulator_dev *rdev)
 {
 	struct dentry *err_ptr = NULL;
@@ -3916,6 +3924,7 @@ static void rdev_init_debugfs(struct regulator_dev *rdev)
 		pr_err("Error-Bad Function Input\n");
 		goto error;
 	}
+	rdev->debug_consumer = reg;
 
 	reg_ops = rdev->desc->ops;
 	mode = S_IRUGO | S_IWUSR;
@@ -3925,7 +3934,6 @@ static void rdev_init_debugfs(struct regulator_dev *rdev)
 						reg, &reg_enable_fops);
 	if (IS_ERR(err_ptr)) {
 		pr_err("Error-Could not create enable file\n");
-		debugfs_remove_recursive(rdev->debugfs);
 		goto error;
 	}
 
@@ -3940,7 +3948,6 @@ static void rdev_init_debugfs(struct regulator_dev *rdev)
 					rdev->debugfs, reg, &reg_fdisable_fops);
 	if (IS_ERR(err_ptr)) {
 		pr_err("Error-Could not create force_disable file\n");
-		debugfs_remove_recursive(rdev->debugfs);
 		goto error;
 	}
 
@@ -3955,7 +3962,6 @@ static void rdev_init_debugfs(struct regulator_dev *rdev)
 						reg, &reg_volt_fops);
 	if (IS_ERR(err_ptr)) {
 		pr_err("Error-Could not create voltage file\n");
-		debugfs_remove_recursive(rdev->debugfs);
 		goto error;
 	}
 
@@ -3970,7 +3976,6 @@ static void rdev_init_debugfs(struct regulator_dev *rdev)
 						reg, &reg_mode_fops);
 	if (IS_ERR(err_ptr)) {
 		pr_err("Error-Could not create mode file\n");
-		debugfs_remove_recursive(rdev->debugfs);
 		goto error;
 	}
 
@@ -3985,18 +3990,26 @@ static void rdev_init_debugfs(struct regulator_dev *rdev)
 				rdev->debugfs, reg, &reg_optimum_mode_fops);
 	if (IS_ERR(err_ptr)) {
 		pr_err("Error-Could not create optimum_mode file\n");
-		debugfs_remove_recursive(rdev->debugfs);
 		goto error;
 	}
 
+	return;
+
 error:
+	rdev_deinit_debugfs(rdev);
 	return;
 }
+
 #else
+
+static inline void rdev_deinit_debugfs(struct regulator_dev *rdev)
+{
+}
+
 static inline void rdev_init_debugfs(struct regulator_dev *rdev)
 {
-	return;
 }
+
 #endif
 
 /**
@@ -4205,8 +4218,8 @@ void regulator_unregister(struct regulator_dev *rdev)
 	if (rdev->supply)
 		regulator_put(rdev->supply);
 	regulator_proxy_consumer_unregister(rdev->proxy_consumer);
+	rdev_deinit_debugfs(rdev);
 	mutex_lock(&regulator_list_mutex);
-	debugfs_remove_recursive(rdev->debugfs);
 	flush_work(&rdev->disable_work.work);
 	WARN_ON(rdev->open_count);
 	unset_regulator_supplies(rdev);
