115640411c4df9548dc84db1e3e23245c2a7e511
Michael Ellerman <mpe@ellerman.id.au>
2015-09-22 13:08:15 +1000

    scripts/tags.sh: Teach tags about some powerpc macros
    The IO accessors on powerpc are generated using macro fu, ie. out_be32()
    etc. Also there are some debugger related symbols that are macro
    generated. Teach scripts/tags.sh about both.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Michal Marek <mmarek@suse.com>
    Signed-off-by: GreyLeshy <andreiskull@gmail.com>

--- scripts/tags.sh
+++ scripts/tags.sh
@@ -199,6 +199,8 @@ exuberant()
 	--regex-c++='/SETPCGFLAG\(([^,)]*).*/SetPageCgroup\1/'		\
 	--regex-c++='/CLEARPCGFLAG\(([^,)]*).*/ClearPageCgroup\1/'	\
 	--regex-c++='/TESTCLEARPCGFLAG\(([^,)]*).*/TestClearPageCgroup\1/' \
+	--regex-c++='/DEF_MMIO_(IN|OUT)_(X|D)\(([^,]*),\s*[^)]*\)/\3/'	\
+	--regex-c++='/DEBUGGER_BOILERPLATE\(([^,]*)\)/\1/'		\
 	--regex-c='/PCI_OP_READ\((\w*).*[1-4]\)/pci_bus_read_config_\1/' \
 	--regex-c='/PCI_OP_WRITE\((\w*).*[1-4]\)/pci_bus_write_config_\1/' \
 	--regex-c='/DEFINE_(MUTEX|SEMAPHORE|SPINLOCK)\((\w*)/\2/v/'	\
