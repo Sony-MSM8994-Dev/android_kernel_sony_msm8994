96337e5e2c1b997d182f9f0590847fce6b1551d0
Matt Wagantall <mwagantall@cyngn.com>
2015-11-30 14:57:25 -0800

    msm: mdss: optimize DSI command delay semantics
    Some DSI commands require a delay to occur between them and the next
    command sent to the panel. The existing code supports this, but is
    not optimal since it sleeps *after* sending the command requiring the
    delay, rather than immediately *before* the next command.
    
    While this doesn't really matter if the command requiring the delay
    is in the middle of a sequence of commands, it can make quite a big
    difference in scenarios where the last command to be executed for a
    while has a delay associated with it.
    
    A good example is panel unblank/power-up sequences, which often end
    with a command requiring a large delay (ex. 120 ms). Since the next
    command to execute may not occur for a very long time, there should
    be no need to sleep for a full 120 ms.
    
    To improve this, and display turn-on latency as a result, note the
    time at which the next command is allowed to be sent. Prior to
    sending the next command, compare this time with the current time.
    If the required amount of time has already elapsed, skip sleeping
    entirely; otherwise, sleep for the difference.
    
    Change-Id: Ibfe723267dd0abd8f5ebc8cb7bf0c17c6fa640c0
    Signed-off-by: Matt Wagantall <mwagantall@cyngn.com>

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -20,7 +20,7 @@
 #include <linux/slab.h>
 #include <linux/iopoll.h>
 #include <linux/kthread.h>
-
+#include <linux/time.h>
 #include <linux/msm_iommu_domains.h>
 
 #include "mdss.h"
@@ -1345,6 +1345,7 @@ static int mdss_dsi_cmds2buf_tx(struct mdss_dsi_ctrl_pdata *ctrl,
 	struct dsi_cmd_desc *cm;
 	struct dsi_ctrl_hdr *dchdr;
 	int len, wait, tot = 0;
+	struct timespec now_ts;
 
 	tp = &ctrl->tx_buf;
 	mdss_dsi_buf_init(tp);
@@ -1365,6 +1366,13 @@ static int mdss_dsi_cmds2buf_tx(struct mdss_dsi_ctrl_pdata *ctrl,
 
 			wait = mdss_dsi_wait4video_eng_busy(ctrl);
 
+			get_monotonic_boottime(&now_ts);
+			if (timespec_compare(&ctrl->wait_until_ts, &now_ts) > 0) {
+				const struct timespec diff_ts =
+					timespec_sub(ctrl->wait_until_ts, now_ts);
+				usleep(timespec_to_ns(&diff_ts) / NSEC_PER_USEC);
+			}
+
 			mdss_dsi_enable_irq(ctrl, DSI_CMD_TERM);
 			if (use_dma_tpg)
 				len = mdss_dsi_cmd_dma_tpg_tx(ctrl, tp);
@@ -1379,8 +1387,11 @@ static int mdss_dsi_cmds2buf_tx(struct mdss_dsi_ctrl_pdata *ctrl,
 			pr_debug("%s: cmd_dma_tx for cmd = 0x%x, len = %d\n",
 					__func__,  cm->payload[0], len);
 
-			if (!wait || dchdr->wait > VSYNC_PERIOD)
-				usleep(dchdr->wait * 1000);
+			if (!wait || dchdr->wait > VSYNC_PERIOD) {
+				get_monotonic_boottime(&ctrl->wait_until_ts);
+				timespec_add_ns(&ctrl->wait_until_ts,
+						dchdr->wait * NSEC_PER_MSEC);
+			}
 
 			mdss_dsi_buf_init(tp);
 			len = 0;
