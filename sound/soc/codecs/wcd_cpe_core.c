1fe1ced1d5b05d9d7ab18b6d1ea2c01a64bff969
Bhalchandra Gajare <gajare@codeaurora.org>
2015-12-03 18:17:25 -0800

    ASoC: wcd_cpe_core: Fix race conditions during message transfer
    Currently, it is possible there could be race condition when there are
    multiple messages to be sent to CPE. Add changes to fix the possible
    race conditions.
    
    CRs-fixed: 946375
    Change-Id: I6a290c00b08fa3afc64acf40cd339a0203f73d96
    Signed-off-by: Bhalchandra Gajare <gajare@codeaurora.org>
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- sound/soc/codecs/wcd_cpe_core.c
+++ sound/soc/codecs/wcd_cpe_core.c
@@ -1855,6 +1855,7 @@ static int wcd_cpe_cmi_send_lsm_msg(
 	if (CMI_HDR_GET_OBM_FLAG(hdr))
 		wcd_cpe_bus_vote_max_bw(core, true);
 
+	INIT_COMPLETION(session->cmd_comp);
 	ret = cmi_send_msg(message);
 	if (ret) {
 		pr_err("%s: msg opcode (0x%x) send failed (%d)\n",
@@ -1878,8 +1879,6 @@ static int wcd_cpe_cmi_send_lsm_msg(
 
 
 rel_bus_vote:
-
-	INIT_COMPLETION(session->cmd_comp);
 
 	if (CMI_HDR_GET_OBM_FLAG(hdr))
 		wcd_cpe_bus_vote_max_bw(core, false);
@@ -2690,13 +2689,13 @@ static int wcd_cpe_lsm_config_lab_latency(
 		 lab_lat->param.param_id, lab_lat->param.param_size,
 		 pld_size);
 
+	WCD_CPE_GRAB_LOCK(&session->lsm_lock, "lsm");
 	ret = wcd_cpe_cmi_send_lsm_msg(core, session, &cpe_lab_latency);
-	if (ret != 0) {
+	if (ret != 0)
 		pr_err("%s: lsm_set_params failed, error = %d\n",
 		       __func__, ret);
-		return -EINVAL;
-	}
-	return 0;
+	WCD_CPE_REL_LOCK(&session->lsm_lock, "lsm");
+	return ret;
 }
 
 /*
@@ -2874,16 +2873,22 @@ static int wcd_cpe_lsm_lab_enable_disable(
 		 __func__, lab_enable->param.module_id,
 		 lab_enable->param.param_id, lab_enable->param.param_size,
 		 pld_size);
+
+	WCD_CPE_GRAB_LOCK(&session->lsm_lock, "lsm");
 	ret = wcd_cpe_cmi_send_lsm_msg(core, session, &cpe_lab_enable);
 	if (ret != 0) {
 		pr_err("%s: lsm_set_params failed, error = %d\n",
 			__func__, ret);
-		return -EINVAL;
+		WCD_CPE_REL_LOCK(&session->lsm_lock, "lsm");
+		goto done;
 	}
+	WCD_CPE_REL_LOCK(&session->lsm_lock, "lsm");
+
 	if (lab_enable->enable)
-		wcd_cpe_lsm_config_lab_latency(core, session,
+		ret = wcd_cpe_lsm_config_lab_latency(core, session,
 					       WCD_CPE_LAB_MAX_LATENCY);
-	return 0;
+done:
+	return ret;
 }
 
 static int wcd_cpe_lsm_control_lab(void *core_handle,
@@ -2945,12 +2950,14 @@ static int wcd_cpe_lsm_eob(
 		0, CPE_LSM_SESSION_CMD_EOB)) {
 		return -EINVAL;
 	}
+
+	WCD_CPE_GRAB_LOCK(&session->lsm_lock, "lsm");
 	ret = wcd_cpe_cmi_send_lsm_msg(core, session, &lab_eob);
-	if (ret != 0) {
+	if (ret != 0)
 		pr_err("%s: lsm_set_params failed\n", __func__);
-		return -EINVAL;
-	}
-	return 0;
+	WCD_CPE_REL_LOCK(&session->lsm_lock, "lsm");
+
+	return ret;
 }
 
 /*
