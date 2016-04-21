86ea05812ef89111d5b1be6770a5567a7d44931e
Fenglin Wu <fenglinw@codeaurora.org>
2016-04-21 12:04:28 +0800

    power_supply: Add SOC_REPORTING_READY property
    Add SOC_REORTING_READY property to indicate if the reporting SOC is the
    actual battery capacity.
    
    CRs-Fixed: 1001496
    Change-Id: Ie0b17be6c9c4f2bf00d9902a597f4d41c7311857
    Signed-off-by: Fenglin Wu <fenglinw@codeaurora.org>
    Signed-off-by: GreyLeshy <andreiskull@gmail.com>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -196,6 +196,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_FV_CFG,
 	POWER_SUPPLY_PROP_FV_CMP_CFG,
 	POWER_SUPPLY_PROP_BATT_AGING,
+	POWER_SUPPLY_PROP_SOC_REPORTING_READY,
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
