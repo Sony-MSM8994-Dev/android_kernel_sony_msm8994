880abbaf3445546f1ed631f24738c5b7b93a02a4
jollaman999 <admin@jollaman999.com>
2016-05-21 20:48:23 +0900

    arp, neighbour: Print ARP informations and add NUD_STATE related URL

--- net/core/neighbour.c
+++ net/core/neighbour.c
@@ -1109,6 +1109,7 @@ int neigh_update(struct neighbour *neigh, const u8 *lladdr, u8 new,
 		goto out;
 	}
 
+	/* arp_project */
 	/* Compare new lladdr with cached one */
 	if (!dev->addr_len) {
 		/* First case: device needs no address. */
