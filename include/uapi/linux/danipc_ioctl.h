2194b1cc6e700c8e5cfec7dd304cabf2a77777cb
Sivanesan Rajapupathi <srajap@codeaurora.org>
2016-07-17 22:31:17 -0400

    net: danipc: Implement the character device interface
    In order to avoid a memory copy in the data path with the network device
    interface, a character device interface is implemented to boost the
    performance.
    
    Change-Id: I198b898a4a03228cfb197c3f8063f6a63c815d47
    Acked-by: Jie Luo <jluo@qti.qualcomm.com>
    Signed-off-by: Sivanesan Rajapupathi <srajap@codeaurora.org>

--- include/uapi/linux/danipc_ioctl.h
+++ include/uapi/linux/danipc_ioctl.h
@@ -1,22 +1,23 @@
 /*
-	All files except if stated otherwise in the begining of the file
-	are under the ISC license:
-	----------------------------------------------------------------------
-
-	Copyright (c) 2010-2012 Design Art Networks Ltd.
-
-	Permission to use, copy, modify, and/or distribute this software for any
-	purpose with or without fee is hereby granted, provided that the above
-	copyright notice and this permission notice appear in all copies.
-
-	THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-	WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-	MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-	ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-	WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-	ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-	OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *	All files except if stated otherwise in the beginning of the file
+ *	are under the ISC license:
+ *	----------------------------------------------------------------------
+ *	Copyright (c) 2016, The Linux Foundation. All rights reserved.
+ *	Copyright (c) 2010-2012 Design Art Networks Ltd.
+ *
+ *	Permission to use, copy, modify, and/or distribute this software for any
+ *	purpose with or without fee is hereby granted, provided that the above
+ *	copyright notice and this permission notice appear in all copies.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *	WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *	MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *	ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *	WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *	ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *	OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
+
 
 
 #ifndef __UAPI_LINUX_DANIPC_IOCTL_H__
@@ -27,6 +28,7 @@
 #define DANIPC_IOCS_REGISTER	(SIOCDEVPRIVATE + 0)
 #define DANIPC_IOCG_ADDR2NAME	(SIOCDEVPRIVATE + 1)
 #define DANIPC_IOCG_NAME2ADDR	(SIOCDEVPRIVATE + 2)
+#define DANIPC_IOCS_MMSGSEND	(SIOCDEVPRIVATE + 3)
 
 #define MAX_AGENTS		256
 #define MAX_AGENT_NAME		32
@@ -59,4 +61,27 @@ struct danipc_name {
 };
 
 #define DANIPC_MAX_BUF 1600
+#define DANIPC_BUFS_MAX_NUM_BUF 8
+
+struct danipc_cdev_msghdr {
+	uint8_t		dst;
+	uint8_t		src;
+	uint16_t	prio;
+} __packed;
+
+struct danipc_buf_entry {
+	void		*data;
+	unsigned	data_len;
+};
+
+struct danipc_bufs {
+	unsigned		num_entry;
+	struct danipc_buf_entry entry[DANIPC_BUFS_MAX_NUM_BUF];
+};
+
+struct danipc_cdev_mmsg {
+	struct danipc_cdev_msghdr	hdr;
+	struct danipc_bufs		msgs;
+};
+
 #endif /* __UAPI_LINUX_DANIPC_IOCTL_H__ */
