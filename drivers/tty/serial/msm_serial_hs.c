0578c00af9266cc5def6dfb9a666421a0cd14e8b
Girish Mahadevan <girishm@codeaurora.org>
2016-08-01 16:51:49 -0600

    serial: msm_serial_hs: Change the wakeup interrupt enable sequence
    The wakeup variable keeps track of the 2 edge interrupts needed to detect
    wakeup from deep sleep, when the second edge interrupt is detected the
    wake byte is injected into the tty buffer.
    On some boards due to noisy GPIO pins an interrupt is detected as soon as
    the wakeup irq is enabled and before the variable is reset leading to
    bogus wakeup bytes being injected into the serial buffer.
    To protect against this, make sure the wakeup interrupt is enabled after
    all the state variables are reset to protect against spurious wakeups.
    
    Change-Id: I8002a67d8d9cb41709049ff34f35717f3dd0acd0
    Acked-by: Yijiang Yuan <yijiangy@qti.qualcomm.com>
    Signed-off-by: Girish Mahadevan <girishm@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    [Resolved context conflicts]
    Signed-off-by: GreyLeshy <andreiskull@gmail.com>

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -2176,12 +2176,12 @@ void toggle_wakeup_interrupt(struct msm_hs_port *msm_uport)
 	if (!(msm_uport->wakeup.enabled)) {
 		MSM_HS_DBG("%s(): Enable Wakeup IRQ", __func__);
 		atomic_set(&msm_uport->wakeup_irq_disabled, 0);
-		enable_irq(msm_uport->wakeup.irq);
-		disable_irq(uport->irq);
 		spin_lock_irqsave(&uport->lock, flags);
 		msm_uport->wakeup.ignore = 1;
 		msm_uport->wakeup.enabled = true;
 		spin_unlock_irqrestore(&uport->lock, flags);
+		disable_irq(uport->irq);
+		enable_irq(msm_uport->wakeup.irq);
 	} else {
 		spin_lock_irqsave(&uport->lock, flags);
 		if (!atomic_read(&msm_uport->wakeup_irq_disabled)) {
