cdc15c118f1302790b22c7b36e7e4b8538fbb855
Dave Weinstein <olorin@google.com>
2017-01-11 16:02:44 -0800

    lib: vsprintf: whitelist stack traces
    Use the %pP functionality to explicitly allow kernel
    pointers to be logged for stack traces
    
    BUG: 30368199
    Change-Id: I495915465565293e9e4da5aa28fbd1d14538d99b
    Signed-off-by: Dave Weinstein <olorin@google.com>
    Signed-off-by: GreyLeshy <andreiskull@gmail.com>

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -141,7 +141,7 @@ static void dump_backtrace(struct pt_regs *regs, struct task_struct *tsk)
 {
 	struct stackframe frame;
 
-	pr_debug("%s(regs = %p tsk = %p)\n", __func__, regs, tsk);
+	pr_debug("%s(regs = %pP tsk = %pP)\n", __func__, regs, tsk);
 
 	if (!tsk)
 		tsk = current;
@@ -208,7 +208,7 @@ static int __die(const char *str, int err, struct pt_regs *regs)
 
 	print_modules();
 	__show_regs(regs);
-	pr_emerg("Process %.*s (pid: %d, stack limit = 0x%p)\n",
+	pr_emerg("Process %.*s (pid: %d, stack limit = 0x%pP)\n",
 		 TASK_COMM_LEN, tsk->comm, task_pid_nr(tsk),
 		 end_of_stack(tsk));
 
