4f1b09affcb29ce528117ff9579c0d68df3f22bc
Pranav Vashi <neobuddy89@gmail.com>
2017-01-26 01:31:33 +0530

    perf: Clean up spaces and fix mismerge
    Change-Id: Ib45e7822dd94b8ba0ff9511986e7d6d151964d7e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -3752,9 +3752,6 @@ static long perf_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	ctx = perf_event_ctx_lock(event);
 	ret = _perf_ioctl(event, cmd, arg);
 	perf_event_ctx_unlock(event, ctx);
-        ctx = perf_event_ctx_lock(event);
-        ret = _perf_ioctl(event, cmd, arg);
-        perf_event_ctx_unlock(event, ctx);
 
 	return ret;
 }
