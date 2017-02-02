06721c0b529836908e29574232246dfe4a3ae846
Nathan Chancellor <natechancellor@gmail.com>
2017-02-02 16:19:05 -0700

    msm_thermal: use %*pb[l] to print bitmaps including cpumasks and nodemasks
    printk and friends can now format bitmaps using '%*pb[l]'.  cpumask
    and nodemask also provide cpumask_pr_args() and nodemask_pr_args()
    respectively which can be used to generate the two printf arguments
    necessary to format the specified cpu/nodemask.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    (cherry picked from commit a4e82d79cc637c09106cb3299c6c1d48b973815b)

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -415,7 +415,8 @@ static void cpus_previously_online_update(void)
 	cpumask_or(cpus_previously_online, cpus_previously_online,
 		   cpu_online_mask);
 	put_online_cpus();
-	cpulist_scnprintf(buf, sizeof(buf), cpus_previously_online);
+	scnprintf(buf, sizeof(buf), "%*pbl",
+			cpumask_pr_args(cpus_previously_online));
 	pr_debug("%s\n", buf);
 }
 
@@ -1197,8 +1198,9 @@ static int __ref init_cluster_freq_table(void)
 				if (!cpu_set) {
 					cpumask_clear_cpu(_cpu,
 						cpus_previously_online);
-					cpumask_scnprintf(buf, sizeof(buf),
-						cpus_previously_online);
+					scnprintf(buf, sizeof(buf), "%*pb",
+						cpumask_pr_args(
+						cpus_previously_online));
 					pr_debug("Reset prev online to %s\n",
 						 buf);
 				}
