0ae78b36b93779644bdd18f8b59edbb305c891ef
Maulik Shah <mkshah@codeaurora.org>
2017-02-21 20:44:46 +0530

    genirq: Use irq_set_affinity_locked to change irq affinity
    Currently PM QoS is not getting notifications if irq affinity
    is changed by core isolation with irq_do_set_affinity.
    
    Use irq_set_affinity_locked instead to get notifications when
    updating affinity.
    
    Change-Id: Iab745a23637d9353f730ca77ac7e92cf61b1bf67
    Signed-off-by: Maulik Shah <mkshah@codeaurora.org>

--- kernel/irq/cpuhotplug.c
+++ kernel/irq/cpuhotplug.c
@@ -38,7 +38,7 @@ static bool migrate_one_irq(struct irq_desc *desc)
 	if (!c->irq_set_affinity) {
 		pr_devel_ratelimited("IRQ%u: unable to set affinity\n", d->irq);
 	} else {
-		int r = irq_do_set_affinity(d, affinity, false);
+		int r = irq_set_affinity_locked(d, affinity, false);
 		if (r)
 			pr_devel_ratelimited("IRQ%u: set affinity failed(%d).\n",
 					    d->irq, r);
