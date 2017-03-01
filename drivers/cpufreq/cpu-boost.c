a39360f45bcb1b8ffab646fad043ce7836cc586e
Alex Naidis <alex.naidis@linux.com>
2017-03-01 21:56:09 +0100

    cpu-boost: Rework scheduling setup
    This patch minimizes the latency of an input
    boost by creating a dedicated kworker and scheduling
    it with the realtime policy "SCHED_FIFO".
    We use "MAX_RT_PRIO - 2" to have the input
    boost task preempt userspace RT tasks
    if needed while being preempted by kernel
    internal RT tasks which are scheduled at
    "MAX_RT_PRIO - 1" or higher.
    
    Also since now the cpu-boost wq would
    only handle the work to disable a running
    input boost, which isn't latency critical,
    we break up the cpu-boost wq and move the
    work to disable a running input boost
    to the shared regular wq.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    [@mortezadadgar:fixed conflict due to migration sync]
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>
    Signed-off-by: GreyLeshy <andreiskull@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2017 Paranoid Android for Nextbit Systems Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -23,6 +24,7 @@
 #include <linux/notifier.h>
 #include <linux/cpufreq.h>
 #include <linux/cpu.h>
+#include <linux/kthread.h>
 #include <linux/sched.h>
 #include <linux/jiffies.h>
 #include <linux/kthread.h>
@@ -32,6 +34,7 @@
 #include <linux/time.h>
 
 #include "../../kernel/sched/sched.h"
+#include <linux/sched/rt.h>
 
 struct cpu_sync {
 	struct task_struct *thread;
@@ -48,9 +51,8 @@ struct cpu_sync {
 };
 
 static DEFINE_PER_CPU(struct cpu_sync, sync_info);
-static struct workqueue_struct *cpu_boost_wq;
 
-static struct work_struct input_boost_work;
+static struct kthread_work input_boost_work;
 
 static unsigned int boost_ms;
 module_param(boost_ms, uint, 0644);
@@ -76,6 +78,9 @@ static bool sched_boost_active;
 
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
+
+static struct kthread_worker cpu_boost_worker;
+static struct task_struct *cpu_boost_worker_thread;
 
 static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 {
@@ -313,7 +318,7 @@ static int boost_mig_sync_thread(void *data)
 			cpufreq_update_policy(src_cpu);
 		if (cpu_online(dest_cpu)) {
 			cpufreq_update_policy(dest_cpu);
-			queue_delayed_work_on(dest_cpu, cpu_boost_wq,
+			schedule_delayed_work_on(dest_cpu,
 				&s->boost_rem, msecs_to_jiffies(boost_ms));
 		} else {
 			s->boost_min = 0;
@@ -364,7 +369,7 @@ static struct notifier_block boost_migration_nb = {
 	.notifier_call = boost_migration_notify,
 };
 
-static void do_input_boost(struct work_struct *work)
+static void do_input_boost(struct kthread_work *work)
 {
 	unsigned int i, ret;
 	struct cpu_sync *i_sync_info;
@@ -405,8 +410,7 @@ static void do_input_boost(struct work_struct *work)
 			sched_boost_active = true;
 	}
 
-	queue_delayed_work(cpu_boost_wq, &input_boost_rem,
-					msecs_to_jiffies(input_boost_ms));
+	schedule_delayed_work(&input_boost_rem, msecs_to_jiffies(input_boost_ms));
 }
 
 static void cpuboost_input_event(struct input_handle *handle,
@@ -421,10 +425,10 @@ static void cpuboost_input_event(struct input_handle *handle,
 	if ((now - last_input_time) < (input_boost_ms * USEC_PER_MSEC))
 		return;
 
-	if (work_pending(&input_boost_work))
+	if (queuing_blocked(&cpu_boost_worker, &input_boost_work))
 		return;
 
-	queue_work(cpu_boost_wq, &input_boost_work);
+	queue_kthread_work(&cpu_boost_worker, &input_boost_work);
 	last_input_time = ktime_to_us(ktime_get());
 }
 
@@ -509,12 +513,17 @@ static int cpu_boost_init(void)
 {
 	int cpu, ret;
 	struct cpu_sync *s;
+	struct sched_param param = { .sched_priority = MAX_RT_PRIO - 2 };
 
-	cpu_boost_wq = alloc_workqueue("cpuboost_wq", WQ_HIGHPRI, 0);
-	if (!cpu_boost_wq)
+	init_kthread_worker(&cpu_boost_worker);
+	cpu_boost_worker_thread = kthread_run(kthread_worker_fn,
+		&cpu_boost_worker, "cpu_boost_worker_thread");
+	if (IS_ERR(cpu_boost_worker_thread))
 		return -EFAULT;
 
-	INIT_WORK(&input_boost_work, do_input_boost);
+	sched_setscheduler(cpu_boost_worker_thread, SCHED_FIFO, &param);
+
+	init_kthread_work(&input_boost_work, do_input_boost);
 	INIT_DELAYED_WORK(&input_boost_rem, do_input_boost_rem);
 
 	for_each_possible_cpu(cpu) {
