7911ee6b90b54b1e8d497648fdd472a5c4b37b49
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-05-11 22:13:13 +0200

    qpnp-fg: [kitakami] 2% drop fix
    this is the adapted (assumed) fix for too quick battery state dropping
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    (cherry picked from commit 374482a756e393a1dbb8a4fa2bd8389cc424d9d5)

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -5396,7 +5396,7 @@ static int fg_common_hw_init(struct fg_chip *chip)
 
 #ifdef CONFIG_QPNP_FG_EXTENSION
 	rc = fg_mem_masked_write(chip, settings[FG_MEM_DELTA_SOC].address, 0xFF,
-			settings[FG_MEM_DELTA_SOC].value,
+			settings[FG_MEM_DELTA_SOC].value == 1 ? 1 : soc_to_setpoint(settings[FG_MEM_DELTA_SOC].value),
 			settings[FG_MEM_DELTA_SOC].offset);
 #else
 	rc = fg_mem_masked_write(chip, settings[FG_MEM_DELTA_SOC].address, 0xFF,
