846d47bf11329d0dd275bea686b6c1e938c9a9ac
Dorimanx <yuri@bynet.co.il>
2017-07-30 02:22:18 +0200

    Make sure all delayed rcu free inodes are flushed before we destroy cache.
    This fix was added to all FS drivers in 3.6.0 -rc7 and was missing here.
    torvalds/linux@8c0a853
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- fs/exfat/exfat_super.c
+++ fs/exfat/exfat_super.c
@@ -2787,6 +2787,12 @@ static int __init exfat_init_inodecache(void)
 
 static void __exit exfat_destroy_inodecache(void)
 {
+	/*
+	 * Make sure all delayed rcu free inodes are flushed before we
+	 * destroy cache.
+	 */
+	rcu_barrier();
+
 	kmem_cache_destroy(exfat_inode_cachep);
 }
 
