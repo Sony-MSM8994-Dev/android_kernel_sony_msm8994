5de2eb0aa14fb0ea34a7ae8e92fbe8c66ed7dbfd
Sriharsha Allenki <sallenki@codeaurora.org>
2017-08-30 17:03:30 +0530

    usb: gadget: f_gps: Fix infinite loop of response available notifications
    As part of function suspend we are clearing all the
    responses and setting the notify count to 0. If a queued
    request on the interrupt endpoint is not
    completed before function suspend and is completed
    after the next resume, the notify count is decremented
    below 0 and causes an infinite loop of response
    available notifications. Prevent this by dequeuing any
    request on the interrupt endpoint during function suspend.
    
    CRs-Fixed: 2100624
    Change-Id: I75425ae84d0901a0ac34c34db3a7bbb527143540
    Signed-off-by: Sriharsha Allenki <sallenki@codeaurora.org>
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- drivers/usb/gadget/f_gps.c
+++ drivers/usb/gadget/f_gps.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2011-2014, 2016, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2014, 2016-2017, The Linux Foundation.
+ * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -250,6 +251,7 @@ static void gps_purge_responses(struct f_gps *dev)
 
 	pr_debug("%s: port#%d\n", __func__, dev->port_num);
 
+	usb_ep_dequeue(dev->notify, dev->notify_req);
 	spin_lock_irqsave(&dev->lock, flags);
 	while (!list_empty(&dev->cpkt_resp_q)) {
 		cpkt = list_first_entry(&dev->cpkt_resp_q,
@@ -490,6 +492,8 @@ static void gps_notify_complete(struct usb_ep *ep, struct usb_request *req)
 		if (!atomic_read(&dev->ctrl_online))
 			break;
 
+		pr_debug("%s: decrement notify_count:%u\n", __func__,
+				atomic_read(&dev->notify_count));
 		if (atomic_dec_and_test(&dev->notify_count))
 			break;
 
