0b043075a241c3f9ca2683191f46d22168c4a58d
Shaohua Li <shli@fb.com>
2017-08-31 22:09:45 -0700

    BACKPORT: block/loop: set hw_sectors
    Loop can handle any size of request. Limiting it to 255 sectors just
    burns the CPU for bio split and request merge for underlayer disk and
    also cause bad fs block allocation in directio mode.
    
    Bug: 117823094
    Change-Id: Ic4957181433c5a0d15f4cfdbf69dc5558d6dc5bd
    Reviewed-by: Omar Sandoval <osandov@fb.com>
    Reviewed-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Shaohua Li <shli@fb.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 54bb0ade6627a183c211345761ec46e4bf0048fe)
    Signed-off-by: Martijn Coenen <maco@android.com>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -1665,6 +1665,8 @@ static int loop_add(struct loop_device **l, int i)
 	blk_queue_make_request(lo->lo_queue, loop_make_request);
 	lo->lo_queue->queuedata = lo;
 
+	blk_queue_max_hw_sectors(lo->lo_queue, BLK_DEF_MAX_SECTORS);
+
 	disk = lo->lo_disk = alloc_disk(1 << part_shift);
 	if (!disk)
 		goto out_free_queue;
