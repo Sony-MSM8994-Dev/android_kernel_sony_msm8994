4fd9bcd313fa8aedf38bc518fdcf989c3b7f62ca
GreyLeshy <andreiskull@gmail.com>
2017-09-17 22:22:30 +0300

    defconfig: launch opensource driver core_ctl

--- kernel/module.c
+++ kernel/module.c
@@ -1171,6 +1171,9 @@ static int check_version(Elf_Shdr *sechdrs,
 	if(!strncmp("wlan", mod->name, 4))
 		return 0;
 
+	if(!strncmp("core_ctl", mod->name, 8))
+		return 0;
+
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
 		return 1;
