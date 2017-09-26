bbcf525115c5527a1048ae10f12835bfd5ebfbe2
Willem de Bruijn <willemb@google.com>
2017-09-26 12:19:37 -0400

    packet: in packet_do_bind, test fanout with bind_lock held
    [ Upstream commit 4971613c1639d8e5f102c4e797c3bf8f83a5a69e ]
    
    Once a socket has po->fanout set, it remains a member of the group
    until it is destroyed. The prot_hook must be constant and identical
    across sockets in the group.
    
    If fanout_add races with packet_do_bind between the test of po->fanout
    and taking the lock, the bind call may make type or dev inconsistent
    with that of the fanout group.
    
    Hold po->bind_lock when testing po->fanout to avoid this race.
    
    I had to introduce artificial delay (local_bh_enable) to actually
    observe the race.
    
    Fixes: dc99f600698d ("packet: Add fanout support.")
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [@aviraxp]: Port to 3.10 with correct return value

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -2529,16 +2529,18 @@ static int packet_do_bind(struct sock *sk, struct net_device *dev, __be16 proto)
 	struct net_device *dev_curr;
 	__be16 proto_curr;
 	bool need_rehook;
+	int ret = 0;
+
+	lock_sock(sk);
+	spin_lock(&po->bind_lock);
 
 	if (po->fanout) {
 		if (dev)
 			dev_put(dev);
 
-		return -EINVAL;
+		ret = -EINVAL;
+		goto out_unlock;
 	}
-
-	lock_sock(sk);
-	spin_lock(&po->bind_lock);
 
 	proto_curr = po->prot_hook.type;
 	dev_curr = po->prot_hook.dev;
@@ -2572,7 +2574,7 @@ static int packet_do_bind(struct sock *sk, struct net_device *dev, __be16 proto)
 out_unlock:
 	spin_unlock(&po->bind_lock);
 	release_sock(sk);
-	return 0;
+	return ret;
 }
 
 /*
