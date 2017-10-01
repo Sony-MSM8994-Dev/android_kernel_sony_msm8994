1a6b7bb38f343133037b961e5744d46baf059942
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-10-02 01:30:41 +0200

    drivers: wireless: bcm4356: Fix direct references to HZ
    inspired by work from humberos, kudos !
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- drivers/net/wireless/bcmdhd_bcm4356/dhd_linux.c
+++ drivers/net/wireless/bcmdhd_bcm4356/dhd_linux.c
@@ -7356,7 +7356,7 @@ dhd_os_ioctl_resp_wait(dhd_pub_t *pub, uint *condition, bool *pending)
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27))
 	timeout = msecs_to_jiffies(dhd_ioctl_timeout_msec);
 #else
-	timeout = dhd_ioctl_timeout_msec * HZ / 1000;
+	timeout = dhd_ioctl_timeout_msec * msecs_to_jiffies(1);
 #endif
 
 	DHD_PERIM_UNLOCK(pub);
@@ -7387,7 +7387,7 @@ dhd_os_d3ack_wait(dhd_pub_t *pub, uint *condition, bool *pending)
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27))
 	timeout = msecs_to_jiffies(dhd_ioctl_timeout_msec);
 #else
-	timeout = dhd_ioctl_timeout_msec * HZ / 1000;
+	timeout = dhd_ioctl_timeout_msec * msecs_to_jiffies(1);
 #endif
 
 	DHD_PERIM_UNLOCK(pub);
@@ -7765,7 +7765,7 @@ void dhd_wait_for_event(dhd_pub_t *dhd, bool *lockvar)
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27))
 	int timeout = msecs_to_jiffies(IOCTL_RESP_TIMEOUT);
 #else
-	int timeout = (IOCTL_RESP_TIMEOUT / 1000) * HZ;
+	int timeout = IOCTL_RESP_TIMEOUT * msecs_to_jiffies(1);
 #endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27)) */
 
 	dhd_os_sdunlock(dhd);
