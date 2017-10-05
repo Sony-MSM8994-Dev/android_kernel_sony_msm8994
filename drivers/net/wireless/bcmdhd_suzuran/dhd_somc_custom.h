613622d14cc473048e492ccb6176ccbc46e16a1d
GreyLeshy <andreiskull@gmail.com>
2017-10-05 23:47:24 +0300

    net: wireless: bcmdhd_suzuran: Update to 1.141.67.30
    * Sony package version: 34.3.A.0.238 (Xperia X)
    
    Changelog:
       DHD 1.141.67.30 - 2017.8.31
        - Security fix
          - CVE-2017-0786 A-37351060 Added boundary check in wl_escan_handler()
          - CVE-2017-0787 A-37722970 Added boundary check in dhd_pno_process_epno_result()
          - CVE-2017-0788 A-37722328 Added boundary check in dhd_handle_hotlist_scan_evt()
          - CVE-2017-0789 A-37685267 Removed unused SWC feature
          - CVE-2017-0790 A-37357704 Added boundary check in dhd_process_full_gscan_result()
          - CVE-2017-0791 A-37306719 Added event length check in wl_notify_rx_mgmt_frame()
          - CVE-2017-0792 A-37305578 Added event length check in dhd_rtt_event_handler()
          - V2017070601 Added P2P IE length check in parsing routine
    
    Signed-off-by: GreyLeshy <andreiskull@gmail.com>

--- drivers/net/wireless/bcmdhd_suzuran/dhd_somc_custom.h
+++ drivers/net/wireless/bcmdhd_suzuran/dhd_somc_custom.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2014 Sony Mobile Communications Inc.
- * All rights, including trade secret rights, reserved.
+ *
+ * Author: Daisuke Niwa daisuke.x.niwa@sonymobile.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2, as
