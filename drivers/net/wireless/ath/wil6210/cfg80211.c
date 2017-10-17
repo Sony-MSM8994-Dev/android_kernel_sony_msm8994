ffedb4f2e79884007e27f5db15994defe6c27d85
Lior David <liord@codeaurora.org>
2017-10-17 15:44:26 +0300

    wil6210: missing length check in wil_cfg80211_mgmt_tx
    Add a length check in wil_cfg80211_mgmt_tx to detect unsigned integer
    overflow.
    
    Change-Id: I37f988481433a2e1238831980715aef32aa89a85
    Signed-off-by: Lior David <liord@codeaurora.org>
    (cherry picked from commit efd25bb7f1a713446d5caade1470a311a5fa5221)

--- drivers/net/wireless/ath/wil6210/cfg80211.c
+++ drivers/net/wireless/ath/wil6210/cfg80211.c
@@ -517,6 +517,7 @@ int wil_cfg80211_mgmt_tx(struct wiphy *wiphy, struct wireless_dev *wdev,
 			 bool no_cck, bool dont_wait_for_ack,
 			 u64 *cookie)
 {
+	size_t total;
 	struct wil6210_priv *wil = wiphy_to_wil(wiphy);
 	int rc;
 	bool tx_status = false;
@@ -530,7 +531,11 @@ int wil_cfg80211_mgmt_tx(struct wiphy *wiphy, struct wireless_dev *wdev,
 	if (len < sizeof(struct ieee80211_mgmt))
 		return -EINVAL;
 
-	cmd = kmalloc(sizeof(*cmd) + len, GFP_KERNEL);
+	total = sizeof(*cmd) + len;
+	if (total < len)
+		return -EINVAL;
+
+	cmd = kmalloc(total, GFP_KERNEL);
 	if (!cmd) {
 		rc = -ENOMEM;
 		goto out;
@@ -540,7 +545,7 @@ int wil_cfg80211_mgmt_tx(struct wiphy *wiphy, struct wireless_dev *wdev,
 	cmd->len = cpu_to_le16(len);
 	memcpy(cmd->payload, buf, len);
 
-	rc = wmi_call(wil, WMI_SW_TX_REQ_CMDID, cmd, sizeof(*cmd) + len,
+	rc = wmi_call(wil, WMI_SW_TX_REQ_CMDID, cmd, total,
 		      WMI_SW_TX_COMPLETE_EVENTID, &evt, sizeof(evt), 2000);
 	if (rc == 0)
 		tx_status = !evt.evt.status;
