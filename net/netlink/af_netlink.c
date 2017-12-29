f345c4d8808f208131aa6cc54374c64ed2b0424e
GreyLeshy <andreiskull@gmail.com>
2017-12-29 19:57:02 +0300

    netlink: Revert changes made by Sony Dev

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -705,6 +705,12 @@ static void netlink_destroy_callback(struct netlink_callback *cb)
 	kfree(cb);
 }
 
+static void netlink_consume_callback(struct netlink_callback *cb)
+{
+	consume_skb(cb->skb);
+	kfree(cb);
+}
+
 static void netlink_skb_destructor(struct sk_buff *skb)
 {
 #ifdef CONFIG_NETLINK_MMAP
@@ -2468,7 +2474,6 @@ static int netlink_dump(struct sock *sk)
 	struct netlink_callback *cb;
 	struct sk_buff *skb = NULL;
 	struct nlmsghdr *nlh;
-	struct module *module;
 	int len, err = -ENOBUFS;
 	int alloc_size;
 
@@ -2518,11 +2523,10 @@ static int netlink_dump(struct sock *sk)
 	if (cb->done)
 		cb->done(cb);
 	nlk->cb = NULL;
-	module = cb->module;
-	skb = cb->skb;
 	mutex_unlock(nlk->cb_mutex);
-	module_put(module);
-	consume_skb(skb);
+
+	module_put(cb->module);
+	netlink_consume_callback(cb);
 	return 0;
 
 errout_skb:
