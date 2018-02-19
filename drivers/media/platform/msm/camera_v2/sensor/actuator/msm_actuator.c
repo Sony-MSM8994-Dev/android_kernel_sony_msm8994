2ee813d7fda46331125cf5fbb8a4772bb692d759
VijayaKumar T M <vtmuni@codeaurora.org>
2018-02-19 18:04:56 +0530

    msm: sensor: actuator: add null pointer check for i2c array
    Issue:
    i2c_reg_tbl may be null under error condition when set param.
    then, other actuator function still may use the i2c_reg_tbl as null.
    
    Fix:
     Add NULL pointer check for i2c tbl.
    
    CRs-Fixed: 2152401
    Change-Id: Ic6d462e2dbc56b3ed5d115bb728c0b21ea301b93
    Signed-off-by: VijayaKumar T M <vtmuni@codeaurora.org>
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -54,7 +54,7 @@ static int32_t msm_actuator_piezo_set_default_focus(
 	CDBG("Enter\n");
 
 	if (a_ctrl->i2c_reg_tbl == NULL) {
-		pr_err("failed. i2c reg tabl is NULL");
+		pr_err("failed. i2c reg table is NULL");
 		return -EFAULT;
 	}
 
@@ -99,7 +99,7 @@ static void msm_actuator_parse_i2c_params(struct msm_actuator_ctrl_t *a_ctrl,
 	}
 
 	if (a_ctrl->i2c_reg_tbl == NULL) {
-		pr_err("failed. i2c reg tabl is NULL");
+		pr_err("failed. i2c reg table is NULL");
 		return;
 	}
 
@@ -540,7 +540,7 @@ static int32_t msm_actuator_piezo_move_focus(
 	}
 
 	if (a_ctrl->i2c_reg_tbl == NULL) {
-		pr_err("failed. i2c reg tabl is NULL");
+		pr_err("failed. i2c reg table is NULL");
 		return -EFAULT;
 	}
 
@@ -615,7 +615,7 @@ static int32_t msm_actuator_move_focus(
 		return -EFAULT;
 	}
 	if (a_ctrl->i2c_reg_tbl == NULL) {
-		pr_err("failed. i2c reg tabl is NULL");
+		pr_err("failed. i2c reg table is NULL");
 		return -EFAULT;
 	}
 	if (dest_step_pos > a_ctrl->total_steps) {
@@ -716,6 +716,10 @@ static int32_t msm_actuator_bivcm_move_focus(
 	}
 	if ((dir > MOVE_FAR) || (dir < MOVE_NEAR)) {
 		pr_err("Invalid direction = %d\n", dir);
+		return -EFAULT;
+	}
+	if (a_ctrl->i2c_reg_tbl == NULL) {
+		pr_err("failed. i2c reg table is NULL");
 		return -EFAULT;
 	}
 	if (dest_step_pos > a_ctrl->total_steps) {
@@ -1108,6 +1112,7 @@ static int32_t msm_actuator_set_position(
 	uint16_t delay;
 	uint32_t hw_params = 0;
 	struct msm_camera_i2c_reg_setting reg_setting;
+
 	CDBG("%s Enter %d\n", __func__, __LINE__);
 	if (set_pos->number_of_steps <= 0 ||
 		set_pos->number_of_steps > MAX_NUMBER_OF_STEPS) {
@@ -1120,6 +1125,11 @@ static int32_t msm_actuator_set_position(
 		!a_ctrl->func_tbl->actuator_parse_i2c_params ||
 		!a_ctrl->i2c_reg_tbl) {
 		pr_err("failed. NULL actuator pointers.");
+		return -EFAULT;
+	}
+
+	if (a_ctrl->i2c_reg_tbl == NULL) {
+		pr_err("failed. i2c reg table is NULL");
 		return -EFAULT;
 	}
 
