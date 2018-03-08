69148968031853e381d531b71161f5db8ce20e93
Chao Yu <yuchao0@huawei.com>
2018-03-08 14:22:56 +0800

    f2fs: wrap all options with f2fs_sb_info.mount_opt
    This patch merges miscellaneous mount options into struct f2fs_mount_info,
    After this patch, once we add new mount option, we don't need to worry
    about recovery of it in remount_fs(), since we will recover the
    f2fs_sb_info.mount_opt including all options.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    [Resolved trivial merge conflicts]
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -2314,7 +2314,7 @@ static ssize_t f2fs_direct_IO(int rw, struct kiocb *iocb,
 	size_t count = iov_length(iov, nr_segs);
 	int err;
 	/* enum rw_hint hint = iocb->ki_hint;
-	int whint_mode = sbi->whint_mode; */
+	int whint_mode = F2FS_OPTION(sbi).whint_mode; */
 
 	err = check_direct_IO(inode, rw, iov, offset, nr_segs);
 	if (err)
