6e170175edec65ba8f6dd0d7fd92fc7e405077ae
GreyLeshy <andreiskull@gmail.com>
2018-04-23 20:43:25 +0300

    cpufreq: cpu-boost: remove duplicate min in boost_adjust_notify
    We did not apply the patch
     "cpufreq: cpu-boost: Remove migration sync boost"
    
    This reverts commit 4ee016a97c8ba8c.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
- * Copyright (C) 2017 Paranoid Android for Nextbit Systems Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -176,8 +175,6 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 			break;
 
 		min = max(b_min, ib_min);
-
-		min = min(min, policy->max);
 
 		pr_debug("CPU%u policy min before boost: %u kHz\n",
 			 cpu, policy->min);
