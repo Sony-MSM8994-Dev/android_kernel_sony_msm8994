70702ec26dc4fbf6bb8cf8a00ab04a6da57da7fe
GreyLeshy <andreiskull@gmail.com>
2018-05-29 21:52:23 +0300

    net: wireless: bcmdhd_suzuran: Update to 1.141.67.33
    * Sony package version: 34.4.A.2.70 (Xperia X)
    
    Changelog:
       DHD 1.141.67.33 - 2018-03-09
         - CVE-2017-13292 A-70722061 Fix integer overflow in wl_get_assoc_ies
         - CVE-2017-13303 A-71359108 Remove obsoleted wl_cfgvendor_priv_string_handler
    
    Signed-off-by: Andrey Cherepkov <andreiskull@gmail.com>

--- drivers/net/wireless/bcmdhd_suzuran/include/epivers.h
+++ drivers/net/wireless/bcmdhd_suzuran/include/epivers.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1999-2017, Broadcom Corporation
+ * Copyright (C) 1999-2018, Broadcom Corporation
  * 
  *      Unless you and Broadcom execute a separate written software license
  * agreement governing use of this software, this software is licensed to you
@@ -32,17 +32,17 @@
 
 #define	EPI_RC_NUMBER		67
 
-#define	EPI_INCREMENTAL_NUMBER	32
+#define	EPI_INCREMENTAL_NUMBER	33
 
 #define	EPI_BUILD_NUMBER	0
 
-#define	EPI_VERSION		1, 141, 67, 32
+#define	EPI_VERSION		1, 141, 67, 33
 
-#define	EPI_VERSION_NUM		0x018d4320
+#define	EPI_VERSION_NUM		0x018d4321
 
 #define EPI_VERSION_DEV		1.141.67
 
 /* Driver Version String, ASCII, 32 chars max */
-#define	EPI_VERSION_STR		"1.141.67.32 (r)"
+#define	EPI_VERSION_STR		"1.141.67.33 (r)"
 
 #endif /* _epivers_h_ */
