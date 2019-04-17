d8ae364d737f05ef6b11fca4d703c5ac3d54b16c
Elektroschmock <elektroschmock78@googlemail.com>
2019-04-17 21:34:16 +0200

    Revert "oom, PM: make OOM detection in the freezer path raceless"
    This reverts commit fcf72a6727a09c3c7089842c2f68a148c485f022.

--- kernel/exit.c
+++ kernel/exit.c
@@ -507,8 +507,6 @@ static void exit_mm(struct task_struct * tsk)
 	mm_update_next_owner(mm);
 
 	mm_released = mmput(mm);
-	if (test_thread_flag(TIF_MEMDIE))
-		unmark_oom_victim();
 	if (mm_released)
 		set_tsk_thread_flag(tsk, TIF_MM_RELEASED);
 }
